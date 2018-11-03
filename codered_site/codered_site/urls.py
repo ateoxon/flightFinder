d24b6eb5cf169c2317f8d63a29056ee9f4144138
Alberto Ceballos <albertoceballos20@gmail.com>
2018-11-03 17:01:37 -0500

    added sample route and view

--- codered_site/codered_site/urls.py
+++ codered_site/codered_site/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from flights.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
+	path('search/country/<str:fromc>/<str:to>',FlightByCountry.as_view()),
 ]
--- codered_site/flights/views.py
+++ codered_site/flights/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+import json
+from django.views import View
+from django.http import JsonResponse
 # Create your views here.
+
+class FlightByCountry(View):
+	def get(self,request,fromc,to):
+		pass
