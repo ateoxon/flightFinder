fb6d7eb23069125386f8b423475cda816d7a45f9
Nathan Robson <nathan.tyler.robson@gmial.com>
2018-11-04 03:00:38 -0600

    feat(testFlight.js): add input checking

--- codered_site/static/js/testFlight.js
+++ codered_site/static/js/testFlight.js
@@ -13,7 +13,7 @@
         return false;
       }
     }
-  });
+  }); // end scroll-trigger
 
   // Scroll to top button appear
   $(document).scroll(function() {
@@ -23,6 +23,26 @@
     } else {
       $('.scroll-to-top').fadeOut();
     }
-  });
+  }); // end scroll
+
+  $('#submit_btn').click( () => {
+    let departure_country = $('#departureCountry').val();
+    let departure_city = $('#departureCity').val();
+
+    let arrival_country = $('#arrivalCountry').val();
+    let arrival_city = $('#arrivalCity').val();
+
+    if(!departure_country || !departure_city ||
+        !arrival_country || !arrival_city) {
+      alert("Fields can't be empty.");
+      return;
+    }
+
+    $.get('./static/index2.html', (r) => {
+      $('#page-top').empty();
+      $('#page-top').append(r);
+    });
+
+  }); // end submit_btn.click()
 
 })(jQuery); // End of use strict
