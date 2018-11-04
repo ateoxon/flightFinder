cccae5eff8e9c3e391e0137debd8609154760730
Nathan Robson <nathan.tyler.robson@gmial.com>
2018-11-03 21:20:52 -0500

    feat(cityCode.py): add API data fetching

---
+++ codered_site/flights/cityCode.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+# locations is of form city-state-country or city-country
+def getCode(locations):
+    resp = requests.get('https://api.skypicker.com/locations?type=slug&term={0}'.format(locations))
+    if resp.text == '404: Not Found':
+        return None
+
+    return resp.json()['locations'][0]['code']
+
+# accepts city codes retrieved with getCode(locations) for depart and arrive
+def getFlights(depart, arrive):
+    resp = requests.get('https://api.skypicker.com/flights?fly_from={0}&fly_to={1}'.format(depart, arrive))
+    if resp.text == '404: Not Found':
+        return None
+
+    return resp.json()['best_results']
+
